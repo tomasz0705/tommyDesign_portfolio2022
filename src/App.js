3241a2e23ff414da11b363e93463c6fc3e3bdbae
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2022-03-20 19:26:58 +0100

    Added RWD to header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="app">
       <Header/>
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <section className="projects">
+      <h1>
+        <span>_</span>projekty<span>_</span>
+      </h1>
+      <div className="projects__container"></div>
+    </section>
+  );
+}
+
+export default Projects;
